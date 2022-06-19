9beed4bb8a8575ef60a53ee20da12cbaae3fbd45
Thao Nguyen <thaontt1408@gmail.com>
2022-06-18 21:04:53 -0600

    Add books as a hashset to establish connection with book table

--- src/main/java/com/example/spring5webapppractice/domain/Publisher.java
+++ src/main/java/com/example/spring5webapppractice/domain/Publisher.java
@@ -1,9 +1,8 @@
 package com.example.spring5webapppractice.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Publisher {
@@ -17,8 +16,18 @@ public class Publisher {
     private String state;
     private String zip;
 
-
+    @OneToMany
+    @JoinColumn(name = "publisher_id") //Give Hibernate a hint to add in a publisher ID to the book record to track the publisher
+    private Set<Book> books = new HashSet<>();
     public Publisher() {
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
     }
 
     public Long getId() {
