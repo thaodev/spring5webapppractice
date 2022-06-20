afee8fe96598aa3d93aba45db76dc6210f3c0dc9
Thao Nguyen <thaontt1408@gmail.com>
2022-06-20 17:51:01 -0600

    Update the path

--- src/main/java/com/example/spring5webapppractice/controller/BookController.java
+++ src/main/java/com/example/spring5webapppractice/controller/BookController.java
@@ -1,13 +1,15 @@
 package com.example.spring5webapppractice.controller;
 
 import com.example.spring5webapppractice.repositories.BookRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/books")
+
 public class BookController {
     //injecting in an instance of the book repository
     private final BookRepository bookRepository;
@@ -15,7 +17,7 @@ public class BookController {
     public BookController(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
-    @GetMapping("/list")
+    @RequestMapping("/books")
     public String getBooks(Model model) {//Spring is going to provide in a model and using the repository to get a list of books
         //this provides the method a model, a model object
         //add attribute called books to the model, execute book repository.
@@ -25,4 +27,12 @@ public class BookController {
         return "books/list";
     }
 
+    @SpringBootApplication
+    public static class Spring5webapppracticeApplication {
+
+        public static void main(String[] args) {
+            SpringApplication.run(Spring5webapppracticeApplication.class, args);
+        }
+
+    }
 }
